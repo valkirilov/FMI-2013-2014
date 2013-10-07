a766c4704fe9891de2639d5767f06a9e0a94337e
Valentin Kirilov <val.kirilov@gmail.com>
2013-10-07 17:18:34 +0300

    First lecture files. - hello-world.cc

---
+++ UP/2013-09-30/hello-world.cc
@@ -0,0 +1,20 @@
+/*
+ *  Programming basics
+ *  FMI SU "St. Kliment Ohridski"
+ *   
+ *  Valentin Kirilov
+ *  val.kirilov@gmail.com
+ *  valkirilov.com
+ *
+ *  30 September 2013
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Welcome to the C++ world
+	cout << "Hello world :)" << endl;
+	return 0;
+}
